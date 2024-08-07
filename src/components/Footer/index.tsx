7b11f1686bd83bf580f04a3847b50d9399258514
Aires Manlig <amanlig@hotmail.com>
2024-08-07 16:36:35 -0700

    change email to work email

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -56,7 +56,7 @@ const Footer = ({ t }: { t: TFunction }) => {
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:amanlig@hotmail.com">
+              <a href="mailto:amanlig@kodekomplete.com">
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
