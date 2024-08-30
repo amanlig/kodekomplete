fbf60c0dcacdca80f955e8a970c3b88cd90d2c71
Aires Manlig <amanlig@hotmail.com>
2024-08-30 08:36:23 -0700

    wip

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,47 +1,18 @@
 import { lazy } from "react";
 import IntroContent from "../../content/IntroContent.json";
-import AboutContent from "../../content/AboutContent.json";
-import MissionContent from "../../content/MissionContent.json";
-import ServiceContent from "../../content/ServiceContent.json";
-import ContactContent from "../../content/ContactContent.json";
 
 const Container = lazy(() => import("../../common/Container"));
-const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 const ContentBlock = lazy(() => import("../../components/ContentBlock"));
 
 const Home = () => {
   return (
     <Container>
-      <ScrollToTop />
       <ContentBlock
         direction="right"
         title={IntroContent.title}
         content={IntroContent.text}
-        button={IntroContent.button}
-        icon="developer.svg"
+        icon="sail.svg"
         id="intro"
-      />
-      <ContentBlock
-        direction="left"
-        title={AboutContent.title}
-        content={AboutContent.text}
-        //section={AboutContent.section}
-        icon="graphs.svg"
-        id="about"
-      />
-      <ContentBlock
-        direction="right"
-        title={MissionContent.title}
-        content={MissionContent.text}
-        icon="product-launch.svg"
-        id="mission"
-      />
-      <ContentBlock
-        direction="left"
-        title={ServiceContent.title}
-        content={ServiceContent.text}
-        icon="waving.svg"
-        id="product"
       />
     </Container>
   );
